42a6445851173b5144e522f8e46f018a945d9bf9
drasura666 <dr.asura666@gmail.com>
2025-09-06 13:22:26 +0530

    Update chat-interface.tsx

--- client/src/components/chat-interface.tsx
+++ client/src/components/chat-interface.tsx
@@ -128,7 +128,7 @@ export function ChatInterface() {
   ];
 
   return (
-    <div className="flex-1 flex flex-col" data-testid="chat-interface">
+    <div className="flex-1 flex flex-col centered-container" data-testid="chat-interface">
       {/* Chat Header */}
       <div className="bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 px-6 py-4 flex items-center justify-between">
         <div>
